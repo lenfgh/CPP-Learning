6f548b6dfb6ec98e1969ac62f097d58e168e6141
lenfgh <fuguohaolen@163.com>
2024-11-14 00:12:15 +0800

    Add 22

--- BIT Lexue/Week 5/【串处理】串的减法/【串处理】串的减法.cpp
+++ BIT Lexue/Week 5/【串处理】串的减法/【串处理】串的减法.cpp
@@ -2,7 +2,39 @@
 // created by FUGUO 24.11.12
 //
 #include<iostream>
+#include<string>
 #include<queue>
 using namespace std;
 
-queue <char> input;
+queue <char> temp;
+
+string input1,input2;
+
+int main(){
+    
+    cin >> input1 >> input2;
+
+    for(int i=0;input1[i]!='\0';i++){
+        char check = input1[i];
+        bool yn=true;
+        for(int j = 0;input2[j]!='\0';j++){
+            if(check == input2[j]){
+                yn = false;
+                break;
+            }
+        }
+        if(yn){
+            temp.push(check);
+        }
+    }
+
+    for(;!temp.empty();){
+        cout << temp.front();
+        temp.pop();
+    }
+    cout << endl;
+    return 0;
+
+
+}
+
